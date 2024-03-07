add2e3a3d6c485e93df673cd01fb2e50db5354d1
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-03-06 21:07:58 -0700

    updated spike test

--- k6/foundations/spike.js
+++ k6/foundations/spike.js
@@ -1,11 +1,11 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-import { AverageStages } from "./lib/load-options.js"
+import { AverageStages } from "./lib/load-options.js";
 
 const BASE_URL = __ENV.BASE_URL || "http://localhost:3333";
 
 export const options = {
-    stages: AverageStages
+    stages: SpikeStages,
 };
 
 export default function () {
