36cd5f5158eb99401ddff27a739835d905c236ca
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-03-06 21:08:42 -0700

    spike fix

--- k6/foundations/spike.js
+++ k6/foundations/spike.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-import { AverageStages } from "./lib/load-options.js";
+import { SpikeStages } from "./lib/load-options.js";
 
 const BASE_URL = __ENV.BASE_URL || "http://localhost:3333";
 
